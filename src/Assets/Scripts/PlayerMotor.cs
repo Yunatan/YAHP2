b0a8917c8c41be198f311bfc830c233727c2323a
yunatan <unatan77@gmail.com>
2017-07-29 22:27:38 +0300

    initial commit

---
+++ src/Assets/Scripts/PlayerMotor.cs
@@ -0,0 +1,63 @@
+﻿using UnityEngine;
+
+public class PlayerMotor : MonoBehaviour
+{
+    public int Speed;
+    public int JumpForce;
+
+    public LayerMask WhatIsGround;
+
+    private bool grounded;
+
+    private Rigidbody2D playerRigidbody;
+    private BoxCollider2D groundCheck;
+
+    private float currentMovementAxis;
+    private bool jumpPending;
+
+    private void Start()
+    {
+        playerRigidbody = gameObject.GetComponent<Rigidbody2D>();
+        groundCheck = gameObject.transform.Find("Colliders").Find("Ground").gameObject.GetComponent<BoxCollider2D>();
+    }
+
+    private void Update()
+    {
+        HandleInput();
+    }
+
+    private void HandleInput()
+    {
+        if (Input.GetButtonDown("Jump") && grounded)
+        {
+            jumpPending = true;
+        }
+        currentMovementAxis = Input.GetAxisRaw("Horizontal");
+    }
+
+    private void FixedUpdate()
+    {
+        HandleJumping();
+        HandleMovement();
+        PerformGroundCheck();
+    }
+
+    void HandleJumping()
+    {
+        if(jumpPending)
+        {
+            playerRigidbody.AddForce(new Vector2(0, JumpForce));
+            jumpPending = false;
+        }
+    }
+
+    void PerformGroundCheck()
+    {
+        grounded = groundCheck.IsTouchingLayers(WhatIsGround);
+    }
+
+    void HandleMovement()
+    {
+        playerRigidbody.velocity = new Vector2(currentMovementAxis * Speed, playerRigidbody.velocity.y);
+    }
+}
---
+++ src/Assets/Scripts/cameraMovement.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraMovement : MonoBehaviour {
+
+    public float smoothTimeX;
+    public float smoothTimeY;
+    float posX, posY;
+    public Camera maincamera;
+    private Vector2 velocity;
+
+
+    // Use this for initialization
+    void Start () {
+        MoveCamera();
+    }
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        MoveCamera();
+	}
+
+    void MoveCamera()
+    {
+
+        posX = Mathf.SmoothDamp(maincamera.transform.position.x, transform.position.x, ref velocity.x, smoothTimeX);
+        posY = Mathf.SmoothDamp(maincamera.transform.position.y, transform.position.y, ref velocity.y, smoothTimeY);
+
+        maincamera.transform.position = new Vector3(posX, posY, -3f);
+        //maincamera.LookAt(transform);
+    }
+}

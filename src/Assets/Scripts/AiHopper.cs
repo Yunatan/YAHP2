8f11a0b94f4b386c194c3d331d4ff472919d260c
yunatan <unatan77@gmail.com>
2017-07-30 10:01:05 +0300

    anton codereview

---
+++ src/Assets/Scripts/AiHopper.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class AiHopper : MonoBehaviour
+{
+    public float aggroDistance;
+
+    private GameObject player;
+    private float playerDistance;
+    private bool canAttack = true;
+
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        playerDistance = Vector2.Distance(player.transform.position, transform.position);
+        if (playerDistance < aggroDistance && canAttack)
+        {
+            StartCoroutine(Attack());
+        }
+    }
+
+    IEnumerator Attack()
+    {
+        canAttack = false;
+        var rb = gameObject.GetComponent<Rigidbody2D>();
+        yield return new WaitForSeconds(1f);
+        if (player.transform.position.x - transform.position.x < 0)
+        {
+            rb.AddForce(new Vector2(-9000, 5000));
+        }
+        else
+        {
+            rb.AddForce(new Vector2(9000, 5000));
+        }
+        yield return new WaitForSeconds(2f);
+        canAttack = true;
+    }
+}
---
+++ src/Assets/Scripts/HeroDeath.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using UnityEngine;
+
+public class HeroDeath : MonoBehaviour
+{
+    public SpriteRenderer fadeOutSprite;
+    public float fadeSpeed;
+    public GameObject spawnPoint;
+
+    private Animator playerAnimator;
+    private int drawDepth = 5;
+    private float alpha = 1.0f;
+    private int fadeDir = -1;
+
+    void Start()
+    {
+        playerAnimator = gameObject.transform.Find("Sprite").GetComponent<Animator>();
+    }
+
+    void OnGUI()
+    {
+        alpha += fadeDir * fadeSpeed * Time.deltaTime;
+        alpha = Mathf.Clamp01(alpha);
+
+        fadeOutSprite.color = new Color(1, 1, 1, alpha);
+    }
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Respawn"))
+        {
+            //CANCONTROL = FALSE;===================================================
+            StartCoroutine(Fade());
+            playerAnimator.Play("hero_death");
+            StartCoroutine(Respawn());
+        }
+    }
+
+    public float BeginFade(int direction)
+    {
+        fadeDir = direction;
+        return (fadeSpeed);
+    }
+
+    IEnumerator Fade()
+    {
+        float fadeTime = BeginFade(1);
+        yield return new WaitForSeconds(fadeTime);
+    }
+
+    IEnumerator OutOfFade()
+    {
+        float fadeTime = BeginFade(-1);
+        yield return new WaitForSeconds(fadeTime);
+    }
+
+    IEnumerator Respawn()
+    {
+        yield return new WaitForSeconds(1f);
+        GameObject.FindGameObjectWithTag("MainCamera").transform.parent = gameObject.transform;
+        gameObject.transform.position = spawnPoint.transform.position;
+        yield return new WaitForSeconds(1f);
+        StartCoroutine(OutOfFade());
+        GameObject.FindGameObjectWithTag("MainCamera").transform.parent = null;
+        yield return new WaitForSeconds(0.8f);
+        playerAnimator.Play("Idle");
+        //CANCONTROL = TRUE;=========================================================
+    }
+}
